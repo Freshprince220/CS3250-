c3a606a196fa4e6e776aed42f90614625f534e92
Nathaniel O'Neal <34556817+Freshprince220@users.noreply.github.com>
2020-08-24 23:55:33 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+        public static void main(String[] args) {
+            Hello assignment = new Hello();
+            System.out.println(assignment.say());
+        }
+}
+
